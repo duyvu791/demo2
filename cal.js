9bdf9d7435b0bc7a346a990f8e4f1aa7f330c113
vundph10997 <duyvu792001@gmail.com>
2021-03-27 09:48:31 +0700

    Initial commit

---
+++ cal.js
@@ -0,0 +1,12 @@
+exports.tinhTong = function (a,b){
+    return a+b;
+}
+exports.tinhHieu = function (a,b){
+    return a - b;
+}
+exports.tinhTich = function (a,b){
+    return a * b;
+}
+exports.tinhThuong = function (a,b){
+    return a / b;
+}
---
+++ index.js
@@ -0,0 +1,55 @@
+var http = require('http');
+var url = require('url');
+var cal = require('./cal');
+var fs = require('fs');
+
+http.createServer(function (req,res){
+    res.writeHead(200,{'Content-Type' :  'text/html'});
+    var tong = cal.tinhTong(3,5);
+    var url = req.url;
+    if(url == '/'){
+        fs.readFile('index.html',function (error,data){
+            if(error == null){
+                res.write(data);
+                res.end();
+            }else {
+                res.end(error);
+            }
+        });
+    }else if(url == '/insert'){
+        fs.appendFile('text.txt','\nghi vao file',function (error){
+            if(error == null){
+                res.end('Ghi thanh cong')
+            }else{
+                res.end(error);
+            }
+        });
+    }else if(url == '/append'){
+        fs.writeFile('text.txt','ghi vao file lan 2',function (error){
+            if(error == null){
+                res.end('Ghi thanh cong')
+            }else{
+                res.end(error);
+            }
+        });
+    }else if(url == '/unlink'){
+        fs.unlink('text.txt',function (error){
+            if(error == null){
+                res.end('Xoa thanh cong')
+            }else{
+                res.end(error);
+            }
+        });
+    }else if(url == '/rename'){
+        fs.rename('text.txt','text2.txt',function (error){
+            if(error == null){
+                res.end('Rename thanh cong')
+            }else{
+                res.end(error);
+            }
+        });
+    }
+
+}).listen(8080);
+
+
